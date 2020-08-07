b712ae7e1d795a6a10d1d4fe9240dc5e25d2b46c
Nilanjan Biswas <nilanjanmotoxforce@gmail.com>
2020-08-08 02:05:25 +0600

    controller views node_modules app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express 	= require('express');
 var exSession 	= require('express-session');
 var bodyParser 	= require('body-parser');
-var login 		= require('./controller/login');
-var home 		= require('./controller/home');
-var logout 		= require('./controller/logout');
+//var login 		= require('./controller/login');
+//var home 		= require('./controller/home');
+//var logout 		= require('./controller/logout');
 var app 		= express();
 
 //config
@@ -12,12 +12,12 @@ app.set('view engine', 'ejs');
 //middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(exSession({secret: 'my secret value', saveUninitialized: true, resave: false}));
-app.use('/login', login);
-app.use('/logout', logout);
-app.use('/home', home);
+//app.use('/login', login);
+//app.use('/logout', logout);
+//app.use('/home', home);
 
 app.get('/', function(req, res){
-	res.send("this is index page!<br> <a href='/login'> login</a> ");
+	//res.send("this is index page!<br> <a href='/login'> login</a> ");
 });
 
 app.listen(3000, function(){
