daad556dbab625079d8b9d003f715c917f92d330
Nilanjan Biswas <nilanjanmotoxforce@gmail.com>
2020-08-13 01:49:58 +0600

    controller

--- controller/home.js
+++ controller/home.js
@@ -2,86 +2,19 @@ var express = require('express');
 var userModel = require.main.require('./models/user');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('*', function(req, res,next) {
 
-    if (req.session.username != null) {
-        res.render('home/index', { uname: req.session.username });
-    } else {
+    if (req.session.username == null) {
         res.redirect('/login');
+    } else {
+        next();
     }
 });
 
-
-
-
-router.get('/view_users', function(req, res) {
-
-    if (req.session.username != null) {
-        userModel.getAll(function(results) {
-            res.render('home/userlist', { userList: results, username: req.session.username });
-        });
-    } else {
-        res.redirect('/login');
-    }
+router.get('/', function(req, res){
+	res.render('home/index', {uname: req.session.username});
 });
 
-router.get('/edit', function(req, res) {
-    if (req.session.username != null) {
-        res.render('home/update');
-    } else {
-        res.redirect('/login');
-    }
-});
-
-router.get('/create', function(req, res) {
-    if (req.session.username != null) {
-        res.render('home/add');
-    } else {
-        res.redirect('/login');
-    }
-});
-
-router.post('/create', function(req, res) {
-
-    if (req.session.username != null) {
-
-        var user = {
-            username: req.body.username,
-            password: req.body.password,
-            email: req.body.email,
-            type: req.body.type
-        }
-
-        userModel.insert(user, function(status) {
-            if (status) {
-                res.redirect('/home/view_users');
-            } else {
-                res.redirect('/home/create');
-            }
-        });
-    } else {
-        res.redirect('/login');
-    }
-});
-
-
-// router.get('/delete/:id', function(req, res){
-
-// 	userModel.get(req.params.id, function(result){
-// 		res.render('home/delete', {user: result});
-// 	});
-
-// });
-
-// router.post('/delete/:id', function(req, res){
-// 	userModel.delete(req.body.id, function(status){
-// 		if(status){
-// 			res.redirect('/home/view_users');
-// 		}else{
-// 			res.redirect('/home');
-// 		}
-// 	});
-//});
 
 
 module.exports = router;
