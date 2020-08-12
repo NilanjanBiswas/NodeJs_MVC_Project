960c758c78c5c2cb9b5a5b9bf9cc3ee39e1f4ff9
Nilanjan Biswas <nilanjanmotoxforce@gmail.com>
2020-08-13 02:24:25 +0600

    models

--- models/user.js
+++ models/user.js
@@ -26,8 +26,8 @@ module.exports =
 	},
 
 	validate: function(user, callback){
-		var sql = "select * from login where username='"+user.username+"' and password='"+user.password+"'";
-		db.getResults(sql, function(result){
+		var sql = "select * from login where username=? and password=?";
+		db.getResults(sql, [user.username, user.password], function(result){
 			if(result.length > 0){
 				callback(true);
 			}else{
